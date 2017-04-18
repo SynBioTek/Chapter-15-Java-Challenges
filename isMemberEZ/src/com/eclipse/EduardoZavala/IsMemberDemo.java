eee57840c8a8dfad1a188fda66ba002c81023ff8
Eduardo Zavala <edzavala@My.Lonestar.edu>
2017-04-17 23:28:47 -0700

    commit 3 isMember

--- isMemberEZ/src/com/eclipse/EduardoZavala/IsMemberDemo.java
+++ isMemberEZ/src/com/eclipse/EduardoZavala/IsMemberDemo.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
  * This program demonstrates a recursive boolean method named isMember. The
  * method searches an array for a specified value and returns true if the value
  * is found in the array, or false if the value is not found in the array.
- *
  */
 
 public class IsMemberDemo {
@@ -25,9 +24,14 @@ public class IsMemberDemo {
 			// Create a Scanner object for keyboard input.
 			Scanner keyboard = new Scanner(System.in);
 
+			// Get user input
 			String input = keyboard.nextLine();
+
+			// Initialize index at 0
 			int index = 0;
-			boolean result = recursiveSequencialSearch(input, index);
+			// Pass input and index as arguments to isMember method and store
+			// return in result
+			boolean result = isMember(input, index);
 
 			// Display the results.
 			if (result == false) {
@@ -36,28 +40,37 @@ public class IsMemberDemo {
 				System.out.println("CORRECT!, It wouldn't be fajitas without " + input + "!");
 			}
 
-			// Does the user want to search again?
+			// Search again?
 			System.out.println("Try again? " + "(Y or N): ");
 			choice = keyboard.nextLine();
 
 		} while (choice.charAt(0) == 'y' || choice.charAt(0) == 'Y');
 
-		// Close Scanner
-		// keyboard.close();
 	}
 
-	private static boolean recursiveSequencialSearch(String input, int index) {
-
+	/**
+	 * The isMember method uses a recursive sequential search approach to plow
+	 * through a string array in search of matching input from the user.
+	 * 
+	 * @param input
+	 *            user input string
+	 * @param index
+	 *            used to stop recursion and as array marker
+	 * @return boolean value of search
+	 */
+	private static boolean isMember(String input, int index) {
+		// Array of strings
 		String ingredients[] = { "beef", "chicken", "tortillas", "tomato", "lettuce", "hot sauce", "avocado", "cheese",
 				"onion", "garlic", "cumin", "bell  pepper", "oregano", "canola oil", "lemon juice" };
-
+		// Base case
 		if (index == ingredients.length - 1) {
 			return false;
 		}
-		if (ingredients[index].equals(input)) {
-
+		// compare input and strings in ingredients array ignoring case
+		if (ingredients[index].equalsIgnoreCase(input)) {
 			return true;
 		} else
-			return recursiveSequencialSearch(input, ++index);
+			// Recursive method
+			return isMember(input, ++index);
 	}
 }
